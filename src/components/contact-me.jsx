67d2280cbf7e038dcc73048c3374be202cd623d9
Moira <moiracorradini@gmail.com>
2024-01-19 12:54:57 +0000

    bug fixed

---
+++ src/components/contact-me.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ContactMe = () => {
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <p>
+        Feel free to reach out to me if you have any questions or if you'd like to collaborate on a project.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <ul>
+        <li>Email: moira@example.com</li>
+        <li>LinkedIn: linkedin.com/in/moiracorradini</li>
+        <li>GitHub: github.com/moiracorradini</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContactMe;
