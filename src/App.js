298b5c717eb55f8f3d5c629366af0f02381adcda
Moira <moiracorradini@gmail.com>
2024-01-19 12:22:50 +0000

    add navbar need fixing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react';
-import Portfolio from './/components/Work'; // Assuming the Portfolio component is in the same directory
+import Portfolio from './components/portfolio'; // Assuming the Portfolio component is in the same directory
+import CustomNavbar from './components/navbar';
+import AboutMe from './components/about-me';
+import ContactMe from './components/contact-me';
+
+
+
 
 function App() {
+
   return (
-    <div>
-      <h1>Your App</h1>
-      <Portfolio />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <CustomNavbar />
+        <Route exact path="/portfolio" element={<Portfolio />} />
+        <AboutMe />
+        <ContactMe />
+      </Routes>
+    </BrowserRouter>
   );
+
 }
-
-
 
 
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,30 @@
+// CustomNavbar.js
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const CustomNavbar = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/">
+        Moira Corradini
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/about-me">
+            About me
+          </Nav.Link>
+          <Nav.Link as={Link} to="/projects">
+            Projects
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact-me">
+            Contact me
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
