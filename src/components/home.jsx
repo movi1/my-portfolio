0275a8a7bece5382a0b762cdd7c9bded55603466
Moira <moiracorradini@gmail.com>
2024-01-22 12:21:59 +0000

    add image and button

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,27 @@
-// Home.js
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import './home-page.css';
 
-const Home = () => {
+const HomePage = () => {
   return (
-    <div>
-      <h2>Welcome to My Portfolio</h2>
-      <p>
-        Hi, I'm Moira Corradini! This is my portfolio website where I showcase my projects
-        and share information about myself. Feel free to explore and learn more about my work.
-      </p>
+    <div className="container-fluid bg-dark text-light min-vh-100 d-flex align-items-center justify-content-center">
+      <div className="text-center">
+        <img
+          src="/images/movi-avatar-no-bg.png"
+          alt="Moira Corradini"
+          className=" img-fluid mb-3"
+
+        />
+        <h1>Hi there, I'm Moira!</h1>
+        <h4>Web Developer</h4>
+        <Link to="/portfolio" className="btn btn-outline-light mt-3">
+           Projects
+        </Link>
+
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default HomePage;
