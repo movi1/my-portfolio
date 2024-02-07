7e7eee6a49a5fb66f370ba90a733e67426340192
Moira <moiracorradini@gmail.com>
2024-02-07 14:02:50 +0000

    save

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
 import projects from './projectData.jsx';
 import Project from './project.jsx';
-import { Card } from "react-bootstrap";
 import '../styles/portfolio.css';
+
+
 
 
 const Portfolio = () => {
   return (
-    <div className="portfolio-page">
-      <h1>My Portfolio</h1>
-      <div className="projects-container">
+    <div className="portfolio-page d-flex justify-content-center align-items-center">
+      <h1 id="title-portfolio">My Portfolio</h1>
+      <Swiper
+        navigation
+        spaceBetween={20}
+        slidesPerView={1}
+        className="swiper-container" // Add the Bootstrap class to the Swiper container
+      >
         {projects.map((project) => (
-          <Project
-            key={project.id}
-            title={project.title}
-            technologies={project.technologies}
-            description={project.description}
-            image={project.image}
-            link={project.link}
-          />
+          <SwiperSlide key={project.id}>
+            <Project
+              title={project.title}
+              technologies={project.technologies}
+              description={project.description}
+              image={project.image}
+              link={project.link}
+            />
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 };
-
 export default Portfolio;
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import '../styles/portfolio.css';
+import { Col } from 'react-bootstrap';
 
 const Project = ({ id, title, technologies, description, image, link }) => {
   return (
-    <div className="project">
-      <h2 className="project-title">{title}</h2>
-      <div className="technologies">{technologies}</div>
-      <div className="project-inner">
-        <div className="project-front">
-          {id === 6 ? (
-            <>
-              <div className="project-details">{description}</div>
-            
-            </>
-          ) : (
-            <img src={image} alt={title} className="project-image" />
-          )}
-        </div>
-        <div className="project-back">
-          <p className="project-details">{description}</p>
-          <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">
-            View on GitHub
-          </a>
+    <Col xs={12} sm={6} md={6} lg={6} xl={6}  className="d-flex justify-content-center align-items-center">
+      <div className="project">
+        <div className="project-inner">
+          <div className="project-info">
+            <div className="project-front">
+              <h2 className="project-title">{title}</h2>
+              <div className="technologies">{technologies}</div>
+
+
+
+              <img src={image} alt={title} className="project-image" />
+            </div>
+          </div>
+          <div className="project-back">
+            <h2 className="project-title">{title}</h2>
+            <div className="technologies">{technologies}</div>
+            <p className="project-details">{description}</p>
+            <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">
+              View on GitHub
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </Col>
   );
 };
 
