b48eb874f7b758332f2a7cfc6756641aab565b98
Moira <moiracorradini@gmail.com>
2024-02-20 13:38:28 +0000

    save

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -26,7 +26,7 @@ const AboutMe = () => {
         <div className="col-lg-6 mb-4">
           {/* Added mb-4 for margin-bottom */}
           <div className="programming-skills">
-            <h1 className="mt-4">Programming Language Skill:</h1>
+            <h1 className="title-about mt-4">Programming Language Skill:</h1>
             <div className="skill-list">
               {skills.map((skill, index) => (
                 <SkillBar key={index} name={skill.name} level={skill.level} />
@@ -39,11 +39,11 @@ const AboutMe = () => {
           {/* Added mb-4 for margin-bottom */}
           <div className="about-content">
             <h2>About Me</h2>
-            <p>Hey, I'm Moira, a Full Stack Developer who turns ideas into cool web stuff. Recently earned my Full Stack Nano Degree with a solid 91%—yes, I love both the front and the back!</p>
+            <p>Hey, I'm Moira, a junior Full Stack Developer. I recently finished my Full Stack Nano Degree with a distinction!!!</p>
             <p>Proud graduate of a Web Development Bootcamp, where I got hands-on with <span className="font-weight-bold">HTML, CSS, Bootstrap, JavaScript, React, and APIs.</span></p>
             <p>Certified by FreeCodeCamp in Responsive Web Design and JavaScript Algorithms. Explored the world of Data & SQL with Code First Girls.</p>
             <p>Fluent in both English and Italian—I believe coding languages aren't the only ones worth mastering.</p>
-            <p>When I'm not coding, you'll catch me <span className="font-italic">painting on canvas, hitting the weights for a bit of mind-body balance,</span> and baking treats for my two children.</p>
+            <p>Aside from coding, I enjoy <span className="font-italic">painting, staying active,</span> and baking treats for my two children.</p>
             <p>Still in the early stages of my coding adventure, but passionate about continuous learning.</p>
           </div>
         </div>
