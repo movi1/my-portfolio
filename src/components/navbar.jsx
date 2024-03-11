fb5ef337ca777fa7542ed4dac8bfbdb56f4a0607
Moira <moiracorradini@gmail.com>
2024-03-11 14:30:30 +0000

    save

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
@@ -12,8 +12,20 @@ const handleImageClick = () => {
 };
 
 const CustomNavbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleCloseClick = () => {
+    setIsMenuOpen(false);
+  };
+
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+    // Add any additional logic specific to the ScrollLink click
+  };
+
   return (
-    <Navbar className='navbar-custom' variant="light" expand="md">
+    <>
+    <Navbar className={`navbar-custom ${isMenuOpen ? 'open' : ''}`} variant="light" expand="md">
       <Link to="/" className="navbar-brand" name="logo" onClick={handleImageClick}>
         <img
           src="/images/logo-movi-dev.png"
@@ -25,31 +37,34 @@ const CustomNavbar = () => {
       <Navbar.Collapse id="navbar-nav" className="justify-content-end">
         <Nav className="ml-auto">
           <ScrollLink
-            to={sections[0]} smooth={true} duration={500} className="nav-link"
+            to={sections[0]} smooth={true} duration={500} className="nav-link" onClick={handleLinkClick}
           >
             About Me
           </ScrollLink>
 
           <ScrollLink
-            to={sections[1]} smooth={true} duration={500} className="nav-link"
+            to={sections[1]} smooth={true} duration={500} className="nav-link" onClick={handleLinkClick}
           >
             Projects
           </ScrollLink>
 
           <ScrollLink
-            to={sections[2]} smooth={true} duration={500} className="nav-link"
+            to={sections[2]} smooth={true} duration={500} className="nav-link" onClick={handleLinkClick}
           >
             Certificates
           </ScrollLink>
 
           <ScrollLink
-            to={sections[3]} smooth={true} duration={500} className="nav-link"
+            to={sections[3]} smooth={true} duration={500} className="nav-link" onClick={handleLinkClick}
           >
             Contact Me
           </ScrollLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
+      {/* Close overlay */}
+      {isMenuOpen && <div className="close-overlay" onClick={handleCloseClick} />}
+    </>
   );
 };
 
--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-// import 'swiper/css/navigation';
-// import 'swiper/css';
+import { TiArrowLeftOutline, TiArrowRightOutline } from "react-icons/ti";
+
+import 'swiper/css/navigation';
+import 'swiper/css';
 
 import projects from './projectData.jsx';
 import Project from './project.jsx';
@@ -12,9 +13,6 @@ import '../styles/portfolio.css';
 
 
 const Portfolio = () => {
-  const navigationPrevRef = React.useRef(null);
-  const navigationNextRef = React.useRef(null);
-
 
 
   return (
@@ -44,15 +42,22 @@ const Portfolio = () => {
             />
           </SwiperSlide>
         ))}
-        <div className="swiper-button-prev">
-          <FaChevronLeft />
+
+        <div className="swiper-button-prev custom-prev-button">
+
+          <TiArrowLeftOutline />
         </div>
-        <div className="swiper-button-next">
-          <FaChevronRight />
+
+        <div className="swiper-button-next custom-next-button">
+          <TiArrowRightOutline />
+
+
         </div>
+
       </Swiper>
 
     </div>
+
   );
 };
 
