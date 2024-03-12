0aad20bb800c37349758cd5dfb30c05c23fbb8a0
Moira <moiracorradini@gmail.com>
2024-03-12 12:22:14 +0000

    save

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
 import '../styles/navbar.css';
 
 const sections = ["about-me", "portfolio", "certificate", "contact-me"];
@@ -12,62 +14,63 @@ const handleImageClick = () => {
 };
 
 const CustomNavbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleCloseClick = () => {
-    setIsMenuOpen(false);
-  };
-
-  const handleLinkClick = () => {
-    setIsMenuOpen(false);
-    // Add any additional logic specific to the ScrollLink click
-  };
+  
 
   return (
-    <>
-    <Navbar className={`navbar-custom ${isMenuOpen ? 'open' : ''}`} variant="light" expand="md">
+    <Navbar
+      className='navbar-custom'
+      variant="light"
+      expand="md"
+      
+    >
       <Link to="/" className="navbar-brand" name="logo" onClick={handleImageClick}>
-        <img
-          src="/images/logo-movi-dev.png"
-          alt="logo"
-          style={{ width: '100px', height: 'auto' }}
-        />
+        <img src="/images/logo-movi-dev.png" alt="logo" style={{ width: '100px', height: 'auto' }} />
       </Link>
-      <Navbar.Toggle aria-controls="navbar-nav" className="custom-toggler" />
+      <Navbar.Toggle aria-controls="navbar-nav" className="custom-toggler" >
+        <FontAwesomeIcon icon={faHamburger} color='#fff' size='lg'/>
+      </Navbar.Toggle>
       <Navbar.Collapse id="navbar-nav" className="justify-content-end">
         <Nav className="ml-auto">
           <ScrollLink
-            to={sections[0]} smooth={true} duration={500} className="nav-link" onClick={handleLinkClick}
+            
+            to={sections[0]}
+            smooth={true}
+            duration={500}
+            className="nav-link"
           >
             About Me
           </ScrollLink>
-
           <ScrollLink
-            to={sections[1]} smooth={true} duration={500} className="nav-link" onClick={handleLinkClick}
+     
+            to={sections[1]}
+            smooth={true}
+            duration={500}
+            className="nav-link"
           >
             Projects
           </ScrollLink>
-
           <ScrollLink
-            to={sections[2]} smooth={true} duration={500} className="nav-link" onClick={handleLinkClick}
+        
+            to={sections[2]}
+            smooth={true}
+            duration={500}
+            className="nav-link"
           >
             Certificates
           </ScrollLink>
-
           <ScrollLink
-            to={sections[3]} smooth={true} duration={500} className="nav-link" onClick={handleLinkClick}
+        
+            to={sections[3]}
+            smooth={true}
+            duration={500}
+            className="nav-link"
           >
             Contact Me
           </ScrollLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-      {/* Close overlay */}
-      {isMenuOpen && <div className="close-overlay" onClick={handleCloseClick} />}
-    </>
   );
 };
 
 export default CustomNavbar;
-
-
